e1b3835eef5764641ec61408f5fee8ee1aae7cb9
Maximilian Sutton <44649709+HoldMyTyr@users.noreply.github.com>
2020-12-10 16:15:54 -0700

    Add files via upload

---
+++ src/theIsleOfLaeso.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class theIsleOfLaeso{
+
+    	/**
+    	*@Author Maximilian C. Sutton
+    	*@Version 0.01
+    	*@Since 2020-12-09
+    	*/
+
+	boolean run = true;
+
+
+	/**
+	*manages the turn and intitalises one players turn
+	*@param int the players number 
+	*/
+	public static void turn(int tic){
+		
+	}
+
+
+	/**
+	*checks if the win conditition is met (probably at end of turn)
+	*/
+	public static void checkWin(){
+
+	}
+
+	public static void main(String[] args) {
+		
+	}	
+}
