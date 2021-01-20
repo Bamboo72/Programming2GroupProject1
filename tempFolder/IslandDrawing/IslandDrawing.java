4c22d04083f304a253ad6fa8781467b17f397628
Jacob Schwartz <jaschwartz27@gmail.com>
2021-01-20 11:56:13 -0700

    Add files via upload

--- tempFolder/IslandDrawing/IslandDrawing.java
+++ tempFolder/IslandDrawing/IslandDrawing.java
@@ -15,30 +15,44 @@ import javax.swing.JPanel;
 import java.awt.Container;
 
 public class IslandDrawing extends JFrame {
- 
+
+
     public IslandDrawing() {
 
         Container con = this.getContentPane();
 
         JPanel panel = new JPanel();
+        JPanel panel2 = new JPanel();
+        JPanel panel3 = new JPanel();
        // panel.setLayout(null);
-        panel.setBounds(400, 200, 400, 389);
+        panel.setBounds(500, 500, 400, 389); // 400 389
         panel.setBackground(Color.BLUE);
 
+        panel2.setBounds(450, 450, 400, 400); // 400 389
+        panel2.setBackground(Color.RED);
+
+        panel3.setBounds(550, 550, 400, 400); // 400 389
+      //  panel3.setBackground(Color.RED);
+
+        con.add(panel3);
         con.add(panel);
+        con.add(panel2);
 
         // Make ImageIcons and put them on the labels
-       // ImageIcon image = new ImageIcon(getClass().getResource("res/tlink.jpg"));
-        ImageIcon image = new ImageIcon(".//res//tlink.jpg");
+        ImageIcon image = new ImageIcon(getClass().getResource("res/tlink.jpg"));
+      //  ImageIcon image = new ImageIcon(".//res//tlink.jpg");
         ImageIcon image2 = new ImageIcon(getClass().getResource("res/title.png"));
-       
+        ImageIcon image3 = new ImageIcon(getClass().getResource("res/bread.jpg"));
+
         // JLabel label = new JLabel(image);
-        JLabel label = new JLabel();
-        label.setIcon(image);
+        JLabel label = new JLabel(image);
+        //label.setIcon(image);
         JLabel label2 = new JLabel(image2);
-      
+        JLabel label3 = new JLabel(image3);
+
         panel.add(label);
-        panel.add(label2);
+        panel2.add(label2);
+        panel3.add(label3);
 
         // label.setLocation(500, 500);
         // label2.setLocation(500, 1000);
@@ -57,8 +71,9 @@ public class IslandDrawing extends JFrame {
         this.setVisible(true);
     }
 
+
     // This version of IslandDrawing() works
-    /*
+/*
     public IslandDrawing() {
         ImageIcon image = new ImageIcon(getClass().getResource("res/tlink.jpg"));
         ImageIcon image2 = new ImageIcon(getClass().getResource("res/title.png"));
@@ -74,7 +89,7 @@ public class IslandDrawing extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);
     }
-    */
+*/
 
     public static void main(String[] args) {
         new IslandDrawing();
