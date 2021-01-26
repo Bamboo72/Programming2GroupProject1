e71104b2e742f068439f180abef375f21de42a5d
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2021-01-26 11:52:53 -0700

    Update Resource.java

--- src/Resource.java
+++ src/Resource.java
@@ -1,17 +1,21 @@
 /**
  * @author Andrew Blodgett
  * @version 1.0
- * @since 2020-12-9
+ * @since 2021-1-26
  */
 
 
 public class Resource {
-    int type; // Maybe char would be easier to use instead of int
+    char type;
     int amount;
 
     /**
      * The constructor for a resource object
-     * @param type
+     * @param type the type of resource
+     * @param amount the amount of the resource
      */
-    public Resource(int type) {}
+    public Resource(char type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
 }
