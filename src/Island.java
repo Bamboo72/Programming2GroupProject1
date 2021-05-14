f08ec69f59d6cb6f3f61a4f80516e2187731fa6e
andrewblodgett <61248022+andrewblodgett@users.noreply.github.com>
2021-05-13 20:05:48 -0600

    Update Island.java

--- src/Island.java
+++ src/Island.java
@@ -1,6 +1,6 @@
 /**
  * This class is the island/game board.
- * 
+ *
  * @author Andrew Blodgett
  * @version 1.0
  * @since 2021-3-6
@@ -9,24 +9,24 @@
 public class Island {
   String[][] board = {
 /*         0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30 */
-/*0*/    {"w","w","w","w","w","w","w","w","w","w","w","w","r","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"},
-/*1*/    {"w","w","w","w","w","w","w","w","w","w","w","r","l","r","w","r","w","r","w","r","w","w","w","w","w","r","w","r","w","w","w"},
-/*2*/    {"w","w","r","w","r","w","w","w","w","w","r","l","r","l","r","l","r","l","r","l","r","w","r","w","r","l","r","l","r","w","w"},
-/*3*/    {"w","r","l","r","l","r","w","r","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w"},
-/*4*/    {"w","w","r","l","r","l","r","l","r","l","r","l","r","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","w"},
-/*5*/    {"w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","r","w","w","w"},
-/*6*/    {"w","w","r","l","r","l","r","l","r","l","r","w","r","w","r","l","r","l","r","l","r","w","r","w","r","w","w","w","w","w","w"},
-/*7*/    {"w","w","w","r","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","w","w","w","w","r","w","r","w","w","w"},
-/*8*/    {"w","w","w","w","w","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","r","w","r","l","r","l","r","w","w"},
-/*9*/    {"w","w","w","w","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","w","w"},
-/*10*/   {"w","w","w","w","r","l","r","l","r","w","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","l","r","w","w"},
-/*11*/   {"w","w","w","r","l","r","l","r","l","r","w","r","l","r","w","r","w","r","w","r","l","r","l","r","w","r","w","r","w","w","w"},
-/*12*/   {"w","w","w","w","r","w","r","w","r","w","w","w","r","w","w","w","w","w","w","w","r","w","r","w","w","w","w","w","w","w","w"}
-  }; /* w = water, r = resource, l = land */
+/*0*/    {"o","o","o","o","o","o","o","o","o","o","o","o","r","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o"},
+/*1*/    {"o","o","o","o","o","o","o","o","o","o","o","r","v","r","o","r","o","r","o","r","o","o","o","o","o","r","o","r","o","o","o"},
+/*2*/    {"o","o","r","o","r","o","o","o","o","o","r","m","r","m","r","h","r","f","r","f","r","o","r","o","r","p","r","f","r","o","o"},
+/*3*/    {"o","r","w","r","w","r","o","r","o","r","h","r","m","r","m","r","h","r","f","r","w","r","w","r","p","r","v","r","v","r","o"},
+/*4*/    {"o","o","r","w","r","w","r","p","r","f","r","h","r","o","r","h","r","h","r","w","r","w","r","w","r","w","r","p","r","o","o"},
+/*5*/    {"o","r","l","r","w","r","w","r","f","r","f","r","l","r","m","r","w","r","w","r","w","r","w","r","w","r","o","r","o","o","o"},
+/*6*/    {"o","o","r","m","r","w","r","w","r","f","r","o","r","o","r","m","r","w","r","w","r","o","r","o","r","o","o","o","o","o","o"},
+/*7*/    {"o","o","o","r","o","r","f","r","w","r","f","r","p","r","m","r","m","r","h","r","o","o","o","o","o","r","o","r","o","o","o"},
+/*8*/    {"o","o","o","o","o","o","r","f","r","f","r","f","r","p","r","h","r","m","r","m","r","o","r","o","r","f","r","h","r","o","o"},
+/*9*/    {"o","o","o","o","o","r","f","r","f","r","f","r","p","r","p","r","h","r","m","r","h","r","f","r","f","r","l","r","o","o","o"},
+/*10*/   {"o","o","o","o","r","f","r","f","r","o","r","v","r","p","r","p","r","f","r","h","r","f","r","f","r","h","r","h","r","o","o"},
+/*11*/   {"o","o","o","r","f","r","l","r","f","r","o","r","p","r","o","r","o","r","o","r","f","r","f","r","o","r","o","r","o","o","o"},
+/*12*/   {"o","o","o","o","r","o","r","o","r","o","o","o","r","o","o","o","o","o","o","o","r","o","r","o","o","o","o","o","o","o","o"}
+}; /* o = ocean, r = resource, l = land, v = village, m = mountain, h = hills, f = forest, p = plains, w = waste  */
 
   /**
    * Returns the board representing the island
-   * 
+   *
    * @return the game board
    */
   public String[][] getBoard() {
@@ -35,20 +35,22 @@ public class Island {
 
   /**
    * The method used to remove an element from the game board
-   * 
-   * @param x
-   * @param y
+   * @param x, y
+   * @param e
    */
-  public void remove(int x, int y) {
+  public void remove(String e, int x, int y) {
+    if(board[y][x].contains(e)) {
+      board[y][x] = board[y][x].substring(0, board[y][x].indexOf(e)) + board[y][x].substring(board[y][x].indexOf(e) + e.length(), board[y][x].length());
+    }
   }
 
   /**
    * the method used to add an element to the game board
-   * 
-   * @param x
-   * @param y
+   * @param x, y
+   * @param e
    */
-  public void add(int x, int y) {
+  public void add(String e, int x, int y) {
+    board[y][x] += e;
   }
 
   // main method for testing
