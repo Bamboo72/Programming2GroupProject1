ffe472fb277c245826c4754796eef578a32b2c6b
Jacob Schwartz <jaschwartz27@gmail.com>
2021-05-21 21:11:43 -0600

    Add files via upload

--- src/Island.java
+++ src/Island.java
@@ -1,177 +1,253 @@
-import java.util.*;
-import java.io.*;
+/**
+ * This class is the island/game board.
+ *
+ * @author Andrew Blodgett
+ * @version 1.0
+ * @since 2021-3-6
+ */
+import java.io.FileNotFoundException;
+import java.util.Random;
 
-public class TheIsleOfLaeso{
+public class Island {
+    public double resourceSpawnRate = 0.1;
+    String[][] board = {
+  /*         0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30 */
+  /*0*/    {"o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","r ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o "},
+  /*1*/    {"o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","o ","r ","v ","r ","o ","r ","o ","r ","o ","r ","o ","o ","o ","o ","o ","r ","o ","r ","o ","o ","o "},
+  /*2*/    {"o ","o ","r ","o ","r ","o ","o ","o ","o ","o ","r ","m ","r ","m ","r ","c ","r ","f ","r ","f ","r ","o ","r ","o ","r ","p ","r ","f ","r ","o ","o "},
+  /*3*/    {"o ","r ","w ","r ","w ","r ","o ","r ","o ","r ","c ","r ","m ","r ","m ","r ","c ","r ","f ","r ","w ","r ","w ","r ","p ","r ","v ","r ","v ","r ","o "},
+  /*4*/    {"o ","o ","r ","w ","r ","w ","r ","p ","r ","f ","r ","c ","r ","o ","r ","c ","r ","c ","r ","w ","r ","w ","r ","w ","r ","w ","r ","p ","r ","o ","o "},
+  /*5*/    {"o ","r ","l ","r ","w ","r ","w ","r ","f ","r ","f ","r ","l ","r ","m ","r ","w ","r ","w ","r ","w ","r ","w ","r ","w ","r ","o ","r ","o ","o ","o "},
+  /*6*/    {"o ","o ","r ","m ","r ","w ","r ","w ","r ","f ","r ","o ","r ","o ","r ","m ","r ","w ","r ","w ","r ","o ","r ","o ","r ","o ","o ","o ","o ","o ","o "},
+  /*7*/    {"o ","o ","o ","r ","o ","r ","f ","r ","w ","r ","f ","r ","p ","r ","m ","r ","m ","r ","c ","r ","o ","o ","o ","o ","o ","r ","o ","r ","o ","o ","o "},
+  /*8*/    {"o ","o ","o ","o ","o ","o ","r ","f ","r ","f ","r ","f ","r ","p ","r ","c ","r ","m ","r ","m ","r ","o ","r ","o ","r ","f ","r ","c ","r ","o ","o "},
+  /*9*/    {"o ","o ","o ","o ","o ","r ","f ","r ","f ","r ","f ","r ","p ","r ","p ","r ","c ","r ","m ","r ","c ","r ","f ","r ","f ","r ","l ","r ","o ","o ","o "},
+  /*10*/   {"o ","o ","o ","o ","r ","f ","r ","f ","r ","o ","r ","v ","r ","p ","r ","p ","r ","f ","r ","c ","r ","f ","r ","f ","r ","c ","r ","c ","r ","o ","o "},
+  /*11*/   {"o ","o ","o ","r ","f ","r ","l ","r ","f ","r ","o ","r ","p ","r ","o ","r ","o ","r ","o ","r ","f ","r ","f ","r ","o ","r ","o ","r ","o ","o ","o "},
+  /*12*/   {"o ","o ","o ","o ","r ","o ","r ","o ","r ","o ","o ","o ","r ","o ","o ","o ","o ","o ","o ","o ","r ","o ","r ","o ","o ","o ","o ","o ","o ","o ","o "}
+}; /* o = ocean, r = resource, l = land, v = village, m = mountain, c = crags, f = forest, p = plains, w = waste  */
 
-  /**
-  *@Author Maximilian C. Sutton (most of main, Character Creation class, Player class, Settings class, Settings files)
-  *@Author Jacob Schwartz (put 100000 hr in, Graphics class, Cool Art, Island layout, Buttons/Mouse class, some of main)
-  *@Author Andrew Blodgett (Resource Generation Class, Dice class, Island class, Structure/Build class)
-  *@Author Brandon Winters (Catastrophe Modeclasses: NaturalEventGenerator class, EnhancedNaturalEventGenerator class)
-  *@Version 0.91
-  *@Since 2021-5-20
-  */
-
-  //graphics, dice, settings, and island objects 
-  static Graphics g = new Graphics();
-  static Dice dice = new Dice(6);
-  static IOSettings s = new IOSettings();
-  static Island i = new Island();
-
-  //turn vars
-  static int moveLeft;
-  static int collectsLeft;
-
-  //to make new player objects that dont crash the game
-  static int[] inventory = new int[6];
-  static String name = "duhadway";
-  static String hat = "Casual";
-  static String clothes = "Casual";
-  static String color = "Red";
-  static String magic = "magic";
-  static String people = "people";
-  static String food = "food";
- 
-  //more turn vars
-  static int numOfPlayer = 4;
-
-  static int playerTurn = 1;
- 
-  static int numAlive = 2;
-
-  static int winType = 0; 
-
-  //an arry of player objects to access ez
-  //static ArrayList<Player> players = new ArrayList<Player>();
-  static Player[] players = new Player[numOfPlayer];
-  static ArrayList<Structure> structures = new ArrayList<Structure>();
-
-  //empty player objects
-  static Player a;
-  static Player b;
-  static Player c;
-  static Player d;
-
-  
-  /**
-  * checks all the win cons, kill, boat, and magic
-  * @pram int numOfP
-  * @retunrs int winType
-  */
-  public static int checkWin(int numOfP){
-    int whichPlayerToCheck; 
-    if(playerTurn == 1){ 
-      whichPlayerToCheck = numOfP - 1;
-    } else {
-      whichPlayerToCheck = numOfP - 2;
-    }
-
-    System.out.println("The player turn is now " + playerTurn + " so it's " + players[playerTurn - 1].getName() + "'s turn, but we are doing a win check for " + players[whichPlayerToCheck].getName() );
-    System.out.print("Win Check: ");
-    //the kill win
-    numAlive = numOfP;
-    if(isDead(Player[0]) == true){
-      numAlive--;
-      System.out.println("1 is dead");
-    }
-    if(isDead(Player[1]) == true){
-      numAlive--;
-      System.out.println("2 is dead");
-    }
-    if(numOfP == 4) {
-      if(isDead(Player[2]) == true){
-        numAlive--;
-        System.out.println("3 is dead");
-      }
-      if(isDead(Player[3]) == true){
-        numAlive--;
-	System.out.println("4 is dead");
-      }
-    } else if(numOfP == 3){
-      if(isDead(Player[2]) == true){
-        numAlive--;
-	System.out.println("3 is dead");
+  public Island() {
+    try {
+      if (IOSettings.findSpawnRate().equals("SpawnRateLow")) {
+        resourceSpawnRate = 0.05;
+      } else if (IOSettings.findSpawnRate().equals("SpawnRateHigh")) {
+        resourceSpawnRate = 0.2;
+      } else {
+        resourceSpawnRate = 0.1;
       }
     }
-    if(numAlive <= 1){
-      winType = 1;
-      System.out.println("KILL WIN!!");
+    catch (FileNotFoundException e){}
+  }
+
+    public void reset() {
+      for (int i = 0; i < board.length; i++) {
+        for (int j = 0; j < board[i].length; j++) {
+          remove(board[i][j].substring(2), j, i);
+        }
+      }
+    }    
+
+    /**
+     * Returns the board representing the island
+     *
+     * @return the game board
+     */
+    public String[][] getBoard() {
+      return board;
     }
 
-    //the magic win
-    if(players[whichPlayerToCheck].getInven()[5] > 8){
-      winType = 2;
-      System.out.println("MAGIC WIN!!");
+    /**
+     * The method used to remove an element from the game board
+     * @param x, y
+     * @param e
+     */
+    public void remove(String e, int x, int y) {
+      if(board[y][x].contains(e)) {
+        board[y][x] = board[y][x].substring(0, board[y][x].indexOf(e)) + board[y][x].substring(board[y][x].indexOf(e) + e.length(), board[y][x].length());
+      } else {
+        System.out.println("Uhhhhhh... This is awkward... I guess that element wasn't actually there so I couldn't remove it. You have drawn the ire of the gods; prepare to be smitten.");
+      }
     }
 
-    //the boat win
-    if(players[whichPlayerToCheck].getResource(people) > 5 && players[whichPlayerToCheck].getResource(food) > 11){ //checks if they have enought recorces
-      
-        boolean boatNearby = false;
-        for(Structure s: structures){
-          if(s.getType() == 'p' && s.owner == whichPlayerToCheck && s.getX() == players[whichPlayerToCheck].getXPos() && s.getY() == players[whichPlayerToCheck].getYPos()){
-            int currentPlayerX = TheIsleOfLaeso.players[whichPlayerToCheck].getXPos();
-            int currentPlayerY = TheIsleOfLaeso.players[whichPlayerToCheck].getYPos();
-            if (s.getY() == currentPlayerY + 1 &&  s.getX() == currentPlayerX + 1) {
-                boatNearby = true;
-            } else if (s.getY() == currentPlayerY + 1 &&  s.getX() == currentPlayerX - 1) {
-              boatNearby = true;
-            } else if (s.getY() == currentPlayerY - 1 &&  s.getX() == currentPlayerX + 1) {
-              boatNearby = true;
-            } else if (s.getY() == currentPlayerY - 1 &&  s.getX() == currentPlayerX - 1) {
-              boatNearby = true;
+    /**
+     * the method used to add an element to the game board
+     * @param x, y
+     * @param e
+     */
+    public void add(String e, int x, int y) {
+      board[y][x] += e;
+    }
+
+    /**
+     * Checks if a certain space on the board contains a string
+     * @param x, y
+     * @param e
+     */
+    public boolean contains(String e, int x, int y) {
+      if(board[y][x].contains(e)) return true;
+      else return false;
+    }
+
+    public void build(char type, int x, int y) {
+      switch(type) {
+        case 's':
+          add("storehouse ", x, y);
+        case 'v':
+          add("village ", x, y);
+        case 'f':
+          add("fort ", x, y);
+        case 'p':
+          add("port ", x, y);
+        case 'b':
+          add("boat ", x, y);
+        default: break;
+      }
+    }
+
+    /**
+     * This method randomly generates resources on resource spaces based on the terrains adjacent to them.
+     */
+    public void resourceGeneration() {
+      for (int y = 0; y < board.length; y++) {
+        for (int x = 0; x < board[y].length; x++) {
+          if (board[y][x].contains("r ")) {
+            double woodChance = 0;
+            double peopleChance = 0;
+            double foodChance = 0;
+            double stoneChance = 0;
+            double oreChance = 0;
+            double magicChance = 0;
+            if (y > 0) {
+              switch(board[y-1][x].charAt(0)) {
+                case 'f':
+                  woodChance += 0.15;
+                  break;
+                case 'm':
+                  oreChance += 0.15;
+                  break;
+                case 'c':
+                  stoneChance += 0.15;
+                  break;
+                case 'v':
+                  peopleChance += 0.27;
+                  break;
+                case 'p':
+                  foodChance += 0.2;
+                  break;
+                case 'l':
+                  magicChance += 0.1;
+                }
+            }
+            if (y < 12) {
+              switch(board[y+1][x].charAt(0)) {
+                case 'f':
+                  woodChance += 0.15;
+                  break;
+                case 'm':
+                  oreChance += 0.15;
+                  break;
+                case 'c':
+                  stoneChance += 0.15;
+                  break;
+                case 'v':
+                  peopleChance += 0.27;
+                  break;
+                case 'p':
+                  foodChance += 0.2;
+                  break;
+                case 'l':
+                  magicChance += 0.1;
+              }
+            }
+            if (x > 0) {
+              switch(board[y][x-1].charAt(0)) {
+                case 'f':
+                  woodChance += 0.15;
+                  break;
+                case 'm':
+                  oreChance += 0.15;
+                  break;
+                case 'c':
+                  stoneChance += 0.15;
+                  break;
+                case 'v':
+                  peopleChance += 0.27;
+                  break;
+                case 'p':
+                  foodChance += 0.2;
+                  break;
+                case 'l':
+                  magicChance += 0.1;
+              }
+            }
+            if (x < 30) {
+              switch(board[y][x+1].charAt(0)) {
+                case 'f':
+                  woodChance += 0.15;
+                  break;
+                case 'm':
+                  oreChance += 0.15;
+                  break;
+                case 'c':
+                  stoneChance += 0.15;
+                  break;
+                case 'v':
+                  peopleChance += 0.27;
+                  break;
+                case 'p':
+                  foodChance += 0.2;
+                  break;
+                case 'l':
+                  magicChance += 0.1;
+              }
+            }
+
+            Random r = new Random();
+            double rand = r.nextDouble();
+            if (rand < woodChance * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "wood ";
+            } else if (rand < (woodChance + peopleChance) * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "people ";
+            } else if (rand < (woodChance + peopleChance + foodChance) * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "food ";
+            } else if (rand < (woodChance + peopleChance + foodChance + stoneChance) * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "stone ";
+            } else if (rand < (woodChance + peopleChance + foodChance + stoneChance + oreChance) * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "ore ";
+            } else if (rand < (woodChance + peopleChance + foodChance + stoneChance + oreChance + magicChance) * resourceSpawnRate) {
+              remove(board[y][x].substring(2), x, y);
+              board[y][x] += "magic ";
             }
           }
         }
-        if(boatNearby){
-          winType = 3;
-          System.out.println("BOAT WIN!!");
-        }
-        
+      }
     }
 
-    if(winType == 0){
-      System.out.println("no win detected");
-    }
-    return winType;
+    // // main method for testing
+    // public static void main(String[] args) {
+    //   Island i = new Island();
+    //   i.resourceGeneration();
+    //   i.resourceGeneration();
+    //   i.resourceGeneration();
+    //   for(String[] s : i.getBoard()){
+    //     for(String ss: s){
+    //       System.out.print(ss + " ");
+    //     }
+    //     System.out.println("");
+    //   }
+    //   i.reset();
+    //   for(String[] s : i.getBoard()){
+    //     for(String ss: s){
+    //       System.out.print(ss + " ");
+    //     }
+    //     System.out.println("");
+    //   }
+    // }
+
   }
-
-  /**
-  * checks to see if a player is passed in is dead
-  * @pram Player player
-  * @retunrs boolean isAreDead
-  */
-  public static boolean isDead(Player player){
-    int h = player.getHealth();
-    boolean isAreDead = false; 
-    if(h <= 0){
-      isAreDead = true;
-    }
-    return isAreDead; 
-  }
-
-  /**
-  * fills the players with things
-  * @pram int numOfP
-  */
-  public static void makeMakeingMeMakeMethods(int numOfP){
-    a = new Player(6, 6, name, hat, clothes, color, inventory, 3);
-    b = new Player(6, 6, name, hat, clothes, color, inventory, 3);
-    players[0] = a;
-    players[1] = b;
-    if(numOfP == 4) {
-      c = new Player(6, 6, name, hat, clothes, color, inventory, 3);
-      d = new Player(6, 6, name, hat, clothes, color, inventory, 3);
-      players[2] = c;
-    players[3] = d;
-    } else if(numOfP == 3){
-      c = new Player(6, 6, name, hat, clothes, color, inventory, 3);
-      players[2] = c;
-    }
-  }
-
-  public static void main(String[] args) throws FileNotFoundException  {
-    g.sceneDisplay(0); 
-    g.refresh();   
-  }	
-}
